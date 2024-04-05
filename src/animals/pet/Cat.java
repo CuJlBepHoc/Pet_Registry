494bb677da4c0f1cd309587f9b9bee5d95e95a22
CuJlBepHoc <cujibephoc@gmail.com>
2024-04-05 17:27:05 +0400

    ver 1.1

--- src/animals/pet/Cat.java
+++ src/animals/pet/Cat.java
@@ -1,4 +1,9 @@
 package animals.pet;
 
-public class Cat {
+import java.time.LocalDate;
+
+public class Cat extends Pet {
+    public Cat(String name, LocalDate birthDate) {
+        super(name, birthDate);
+    }
 }
